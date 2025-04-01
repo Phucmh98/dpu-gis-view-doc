45cbb5d4db10234a3be14427a29b10ff2517c4d5
Phuc <mhphuc.98@gmail.com>
2025-04-01 16:28:14 +0700

    Update Icon

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -11,14 +11,12 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <img
+          src="/asset/img/favicon.ico"  // Đường dẫn tới logo của bạn
+          alt="DPU-logo"
+          width={24}               // Điều chỉnh kích thước của logo
+          height={24}
+        />
         Document
       </>
     ),
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,37 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+ export const metadata: Metadata = {
+	icons: {
+		icon: [
+			{
+				url: '/assets/img/favicon.ico',
+				href: '/assets/img/favicon.ico',
+			},
+			{
+				url: '/assets/img/favicon.ico',
+				href: '/assets/img/favicon.ico',
+			},
+		],
+		shortcut: [
+			{
+				url: '/assets/img/favicon.ico',
+				href: '/assets/img/favicon.ico',
+			},
+			{
+ 				url: '/assets/img/favicon.ico',
+				href: '/assets/img/favicon.ico',
+			},
+		],
+		
+	},
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
